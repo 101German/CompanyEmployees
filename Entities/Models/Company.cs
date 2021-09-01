64d48e777436582b20595dbc29a9fa74e42923a1
German <geras210e@gmail.com>
2021-09-01 21:15:03 +0300

    fixed a mistake in the field name

--- Entities/Models/Company.cs
+++ Entities/Models/Company.cs
@@ -19,7 +19,7 @@ namespace Entities.Models
 
         [Required(ErrorMessage ="Company address is a required field.")]
         [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
-        public string Adress { get; set; }
+        public string Address { get; set; }
         public string Country { get; set; }
         public ICollection<Employee> Employees { get; set; }
     }
