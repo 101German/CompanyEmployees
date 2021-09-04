7d0db8db2e26a0a8e609b3ddd33b2dcafe84d32a
German <geras210e@gmail.com>
2021-09-04 14:35:36 +0300

    Add MappingProfile

---
+++ CompanyEmployees/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Entities.DataTransferObfects;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Mapping
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Company, CompanyDTO>() .ForMember(c => c.FullAddress, 
+                opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+        }
+    }
+}
