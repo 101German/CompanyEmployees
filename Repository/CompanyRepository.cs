7f0dfab4800613122a8b35102fb7eea7a72d8fbf
German <geras210e@gmail.com>
2021-09-04 21:42:27 +0300

    Modify the GetCompany method

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -17,6 +17,6 @@ namespace Repository
         }
 
         public IEnumerable<Company> GetAllCompanies(bool trackChanges) => FindAll(trackChanges).OrderBy(c => c.Name).ToList();
-        public Company GetCompany(Guid companyId, bool trackChanges) => FindByCondition(x => x.Id == companyId, trackChanges).SingleOrDefault();
+        public Company GetCompany(Guid companyId, bool trackChanges) => FindByCondition(x => x.Id.Equals(companyId), trackChanges).SingleOrDefault();
     }
 }
