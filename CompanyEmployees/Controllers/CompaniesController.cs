d810e8a3e8934a590eec621f7937d93ccbb79da9
German <geras210e@gmail.com>
2021-09-04 20:04:52 +0300

    Add the GetCompany method

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -34,6 +34,21 @@ namespace CompanyEmployees.Controllers
                
             
         }
+        [HttpGet("{id}")]
+        public IActionResult GetCompany(Guid id)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            else
+            {
+                var companyDTO = _mapper.Map<CompanyDTO>(company);
+                return Ok(companyDTO);
+            }
+        }
 
     }
 }
