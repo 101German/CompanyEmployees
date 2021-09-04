dbddf171fb82822c748302b94543a7af8d5e57e4
German <geras210e@gmail.com>
2021-09-04 19:02:15 +0300

    Modify the GetCompanies method

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -26,17 +26,13 @@ namespace CompanyEmployees.Controllers
         [HttpGet]
         public IActionResult GetCompanies()
         {
-            try
-            {
+         
                 var companies = _repository.Company.GetAllCompanies(trackChanges: false);
                 var companiesDTO = _mapper.Map<IEnumerable<CompanyDTO>>(companies);
                 return Ok(companiesDTO);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action { ex}"   );
-            return StatusCode(500, "Internal server error");
-            }
+           
+               
+            
         }
 
     }
