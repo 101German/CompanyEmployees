13dc6dd36f0ec83756fa94491da60d8f530086ff
German <geras210e@gmail.com>
2021-09-04 21:41:09 +0300

    Add the GetEmployees method

--- Contracts/IEmployeeRepository.cs
+++ Contracts/IEmployeeRepository.cs
@@ -1,8 +1,12 @@
 ﻿
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+
 namespace Contracts
 {
     public interface IEmployeeRepository
     {
-        
+        IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
     }
 }
