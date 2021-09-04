abdc45ebd7fe41b42762723874c3977500b249ef
German <geras210e@gmail.com>
2021-09-04 23:20:10 +0300

    Add the GetEmployeeForCompany method

--- CompanyEmployees/Controllers/EmployeeController.cs
+++ CompanyEmployees/Controllers/EmployeeController.cs
@@ -38,5 +38,24 @@ namespace CompanyEmployees.Controllers
             return Ok(employeesDTO);
 
         }
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
+            return NotFound();
+            }
+            var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges:false);
+            if (employeeDb == null)
+            {
+                _logger.LogInfo($"Employee with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            var employee = _mapper.Map<EmployeeDTO>(employeeDb);
+            return Ok(employee);
+        }
+
     }
 }
