9b7624d6c0f2df4bebf301bbb5ba3d03035cbb11
German <geras210e@gmail.com>
2021-09-05 21:08:40 +0300

    Implement the GetByIds method

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -18,6 +18,8 @@ namespace Repository
 
         public IEnumerable<Company> GetAllCompanies(bool trackChanges) => FindAll(trackChanges).OrderBy(c => c.Name).ToList();
         public Company GetCompany(Guid companyId, bool trackChanges) => FindByCondition(x => x.Id.Equals(companyId), trackChanges).SingleOrDefault();
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) => FindByCondition(x => ids.Contains(x.Id), trackChanges).ToList();
         public void CreateCompany(Company company) => Create(company);
+
     }
 }
