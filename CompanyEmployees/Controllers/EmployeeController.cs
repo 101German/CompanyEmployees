b58cc72f745e5a85c3ea6eb38d422cbb734c566f
German <geras210e@gmail.com>
2021-09-06 16:41:16 +0300

    Add the UpdateEmployeeForCompany endpoint

--- CompanyEmployees/Controllers/EmployeeController.cs
+++ CompanyEmployees/Controllers/EmployeeController.cs
@@ -28,6 +28,7 @@ namespace CompanyEmployees.Controllers
         [HttpGet]
         public IActionResult GetEmployees(Guid companyId)
         {
+            
             var company = _repository.Company.GetCompany(companyId, trackChanges:false);
             if (company == null)
             {
@@ -99,6 +100,35 @@ namespace CompanyEmployees.Controllers
             }
 
             _repository.Employee.DeleteEmployee(employeeForCompany);
+            _repository.Save();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployeeForCompany(Guid companyId,Guid id,[FromBody] EmployeeForUpdateDTO employee)
+        {
+            if(employee == null)
+            {
+                _logger.LogError("EmployeeForUpdateDTO object sent from client is null");
+                return BadRequest("EmployeeForUpdateDTO object is null");
+            }
+
+            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            if(company == null)
+            {
+                _logger.LogInfo($"Company with id:{companyId} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var employeeEntity = _repository.Employee.GetEmployee(companyId, id, trackChanges: true);
+            if(employeeEntity == null)
+            {
+                _logger.LogInfo($"Employee with id:{id} doesn-t exist in the database");
+                return NotFound();
+            }
+
+            _mapper.Map(employee, employeeEntity);
             _repository.Save();
 
             return NoContent();
