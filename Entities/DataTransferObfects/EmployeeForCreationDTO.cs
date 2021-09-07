3d6a07d2309b85da17870c4e614608f3c658fb76
German <geras210e@gmail.com>
2021-09-07 19:39:13 +0300

    Refactor DTO models

--- Entities/DataTransferObfects/EmployeeForCreationDTO.cs
+++ Entities/DataTransferObfects/EmployeeForCreationDTO.cs
@@ -7,18 +7,8 @@ using System.Threading.Tasks;
 
 namespace Entities.DataTransferObfects
 {
-    public class EmployeeForCreationDTO
+    public class EmployeeForCreationDTO : EmployeeForManipulationDTO
     {
-        [Required(ErrorMessage = "Employee name is a required field.")]
-        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
-        public string Name { get; set; }
-
-        [Range(18, int.MaxValue,ErrorMessage ="Age is required and it can't be lower than 18")]
-        public int Age { get; set; }
-
-        [Required(ErrorMessage = "Position is a required field.")]
-        [MaxLength(30, ErrorMessage = "Maximum length for the Position is 20 characters.")]
-        public string Position { get; set; }
 
     }
 }
