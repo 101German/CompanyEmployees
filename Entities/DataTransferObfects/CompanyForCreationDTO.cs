86362da155f3b50144c7726353d1b96648cededb
German <geras210e@gmail.com>
2021-09-08 23:09:20 +0300

    Modify the CompanyForCreation model

--- Entities/DataTransferObfects/CompanyForCreationDTO.cs
+++ Entities/DataTransferObfects/CompanyForCreationDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,12 @@ namespace Entities.DataTransferObfects
 {
     public class CompanyForCreationDTO
     {
+        [Required(ErrorMessage = "Company name is a required field")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Company address is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
         public string Address { get; set; }
         public string Country { get; set; }
         public IEnumerable<EmployeeForCreationDTO> Employees { get; set; }
