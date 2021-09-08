62a121250877e0cc5f346e13fea823a47953da06
German <geras210e@gmail.com>
2021-09-08 20:59:29 +0300

    Refactor methods of EmployeeRepository

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -1,10 +1,10 @@
 ﻿using Contracts;
 using Entities;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Repository
@@ -16,14 +16,14 @@ namespace Repository
 
         }
 
-        public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) => 
-            FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id),trackChanges)
-            .SingleOrDefault();
+        public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) => 
+           await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id),trackChanges)
+            .SingleOrDefaultAsync();
         
 
-        public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) => 
-            FindByCondition(c => c.CompanyId.Equals(companyId), trackChanges)
-            .OrderBy(e=>e.Name);
+        public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges) => 
+           await FindByCondition(c => c.CompanyId.Equals(companyId), trackChanges)
+            .OrderBy(e=>e.Name).ToListAsync();
        
         public void CreateEmployeeForCompany(Guid companyId,Employee employee)
         {
