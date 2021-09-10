775372c9f0f3d809d3e3d458420446ac419b9142
German <geras210e@gmail.com>
2021-09-10 23:03:58 +0300

    Add parameters

--- Entities/RequestFeaters/EmployeeParameters.cs
+++ Entities/RequestFeaters/EmployeeParameters.cs
@@ -3,6 +3,9 @@ namespace Entities.RequestFeaters
 {
     public class EmployeeParameters:RequestParameters
     {
-        
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set; } = int.MaxValue;
+
+        public bool ValidAgeRange => MaxAge > MinAge;
     }
 }
