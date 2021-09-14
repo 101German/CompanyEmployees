ad32167a3dd6b580ac9feb465debfea7abd5740b
German <geras210e@gmail.com>
2021-09-14 19:39:03 +0300

    Add Authentication Controller

---
+++ CompanyEmployees/Controllers/AuthenticationController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    public class AuthenticationController:ControllerBase
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        public AuthenticationController(ILoggerManager logger,IMapper mapper,UserManager<User> userManager)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+    }
+}
