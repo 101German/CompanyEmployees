624a5f628f6b4ab974a347f37db342e610961e43
German <geras210e@gmail.com>
2021-09-14 19:38:38 +0300

    Add-Migration AddedRolesToDb

---
+++ CompanyEmployees/Migrations/20210914162419_AddedRolesToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompanyEmployees.Migrations
+{
+    public partial class AddedRolesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "64b7429b-ba6f-4370-a8a0-881ea1a8a4ee", "a888b621-78a4-4268-ade7-801d7a42475d", "Manager", "MANAGER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "cf1fa55d-672d-48d7-a9f6-ae6084e63f40", "29741f51-c1eb-4863-9924-b5a4e1af3667", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "64b7429b-ba6f-4370-a8a0-881ea1a8a4ee");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cf1fa55d-672d-48d7-a9f6-ae6084e63f40");
+        }
+    }
+}
