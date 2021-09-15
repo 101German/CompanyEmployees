02d153ace440dba7dd8d8bbf3b6f4c373e648210
German <geras210e@gmail.com>
2021-09-15 21:13:51 +0300

    Add summary for the some endpoints of Company controller

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -32,7 +32,11 @@ namespace CompanyEmployees.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet,Authorize(Roles ="Manager")]
+        /// <summary>
+        /// Gets the list of all companies
+        /// </summary>
+        /// <returns>The companies list</returns>
+        [HttpGet,Authorize(Roles ="Administrator")]
         public async Task<IActionResult> GetCompanies()
         {
 
@@ -76,6 +80,17 @@ namespace CompanyEmployees.Controllers
             return Ok(companiesToReturn);
         }
 
+        /// <summary>
+        /// Creates a newly created company
+        /// </summary>
+        /// <param name="company"></param>
+        /// <returns>A newly created company</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>
+        /// <response code="422">If the model is invalid</response>
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(422)]
         [HttpPost]
         [ServiceFilter(typeof(ValidationFilterAttribute))]
         public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDTO company)
